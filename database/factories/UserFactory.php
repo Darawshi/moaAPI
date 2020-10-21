4ac76e2d660019b8b505191bb84216a6c13e21d5
bassam.darawshi <bassam.darawshi@gmail.com>
2020-10-21 13:48:40 +0300

    trait

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,7 @@ class UserFactory extends Factory
             'name_ar' => $this->faker->name,
             'name_en' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'emp_id' => $this->faker->unique(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
