4eabcb93188589ff80a67065f2779a6ea5404b7c
Bassam <Bassam.darawshi@gmail.com>
2020-10-28 12:20:18 +0200

    adv db

--- app/Http/Controllers/Adv/AdvController.php
+++ app/Http/Controllers/Adv/AdvController.php
@@ -47,46 +47,16 @@ class AdvController extends Controller
         }
     }
 
-    public function create()
-    {
-        //
-    }
-
     public function store(Request $request)
     {
         //
     }
 
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
